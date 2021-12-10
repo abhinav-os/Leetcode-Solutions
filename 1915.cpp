c4d28926b9f2c7ba7a8657a55583e7800858c2eb
Abhinav <abhinavkiran73@gmail.com>
2021-12-10 23:04:35 +0530

    Implemented 1915-Number of Wonderful Substrings

--- 1915.cpp
+++ 1915.cpp
@@ -1,11 +1,38 @@
-// #medium
+// #medium #bitmasking #string
 /*
-    Time Complexity: 
-    Space Complexity: 
+    Time Complexity: O(APLPHABET SIZE * N)
+    Space Complexity: O(N)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+#define ll long long
+
+long long wonderfulSubstrings(string word) {
+    ll cnt = 0, n = word.length();
+    unordered_map<int, int> mp;
+
+    ll mask = 0;
+    mp[mask] = 1;
+
+    for(int i = 0; i < n; i++) {
+        mask = mask xor (1 << (word[i] - 'a'));
+        if(mp[mask] != 0)
+            cnt += mp[mask];
+        
+        for(int j = 0; j < 10; j++) {
+            ll tempMask = mask xor (1 << j);
+            if(mp[tempMask] != 0) 
+                cnt += mp[tempMask];
+        }
+
+        mp[mask] += 1;
+    }
+
+    return cnt;
+}
+
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -13,7 +40,10 @@ int main() {
         freopen("output.txt", "w", stdout);
     #endif
 
-    
+    string str;
+    cin >> str;
+
+    cout << wonderfulSubstrings(str);
 }
 
 /*
