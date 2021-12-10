9b399a1e26ac428e677a6f8f45a930f52840471f
Abhinav <abhinavkiran73@gmail.com>
2021-12-10 18:54:28 +0530

    Initial commit

---
+++ 1915.cpp
@@ -0,0 +1,21 @@
+// #medium
+/*
+    Time Complexity: 
+    Space Complexity: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    
+}
+
+/*
+
+*/
